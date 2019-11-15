173d0584128426b6dc0ca05e6465a2f4254529ac
Mayra Vazquez-Sanchez <mayravsancez479@gmail.com>
2019-11-14 23:41:04 -0500

    Initial Commit

---
+++ Parstagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Parstagram
+//
+//  Created by Mayra Vázquez-Sánchez on 11/14/19.
+//  Copyright © 2019 MVS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
