3ec9e5f7e740f73c7b8aba8808037c50c5040c23
Mayra Vazquez-Sanchez <mayravsancez479@gmail.com>
2019-11-15 23:33:43 -0500

    Initial commit

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Mayra Vázquez-Sánchez on 11/15/19.
+//  Copyright © 2019 MVS. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
